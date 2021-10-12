81f7eedfe60c5c6fae0d970b9eb04948a73032c0
bbnsdevelop <bbnsdevelop@gmail.com>
2021-10-11 21:44:58 -0300

    access variable on object reference

---
+++ java-core/src/br/com/bbnsdevelop/oop/ObjectReferenceDemo.java
@@ -0,0 +1,22 @@
+package br.com.bbnsdevelop.oop;
+
+public class ObjectReferenceDemo {
+
+	int x;
+	
+	ObjectReferenceDemo() {
+
+	}
+	
+	
+	public static void main(String[] args) {
+		ObjectReferenceDemo ord = new ObjectReferenceDemo();
+		ord.x = 10;
+		
+		System.out.println(ord.x);
+	}
+	
+	
+	
+
+}
