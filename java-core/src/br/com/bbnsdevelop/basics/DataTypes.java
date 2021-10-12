e296d4d14d394ddfcc5652a3c7fd7e995c448611
bbnsdevelop <bbnsdevelop@gmail.com>
2021-10-11 21:51:13 -0300

    primitive data types

---
+++ java-core/src/br/com/bbnsdevelop/basics/DataTypes.java
@@ -0,0 +1,28 @@
+package br.com.bbnsdevelop.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		byte b = 1;
+		short s = Short.MAX_VALUE;
+		char c = 'a';
+		int i = Integer.MAX_VALUE;
+		long l = Long.MAX_VALUE;
+		float f = Float.MAX_VALUE;
+		double d = Double.MAX_VALUE;
+		
+		
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		
+		
+		
+	}
+
+}
