19da36fc16125953ce4b2a8dbdc8718976f7ff35
bbnsdevelop <bbnsdevelop@gmail.com>
2021-10-20 22:50:41 -0300

    abstract class

---
+++ java-core/src/br/com/bbnsdevelop/oop/abstracts/BMW.java
@@ -0,0 +1,11 @@
+package br.com.bbnsdevelop.oop.abstracts;
+
+public abstract class BMW {
+	
+	void commonFunc() {
+		System.out.println("Inside commonFunc method");
+	}
+	
+	abstract void accelerate();
+
+}
