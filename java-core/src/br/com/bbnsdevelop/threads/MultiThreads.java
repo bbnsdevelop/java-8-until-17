ac3392195bb64af8dd1e6ffa94c4050987b63293
bbnsdevelop <bbnsdevelop@gmail.com>
2021-11-18 21:41:27 -0300

    sleep thread

--- java-core/src/br/com/bbnsdevelop/threads/MultiThreads.java
+++ java-core/src/br/com/bbnsdevelop/threads/MultiThreads.java
@@ -2,11 +2,12 @@ package br.com.bbnsdevelop.threads;
 
 public class MultiThreads extends Thread {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		MultiThreads mt = new MultiThreads();
 		mt.start();
 		for (int j = 0; j <= 200; j++) {
 			System.out.println("J: " + j +"\t");
+			Thread.sleep(1000);
 			
 		}
 		
@@ -20,7 +21,11 @@ public class MultiThreads extends Thread {
 	void printNumbersI() {
 		for (int i = 0; i <= 200; i++) {
 			System.out.println("i: " + i +"\t");
-			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println("Child Thread exiting");
+			}
 		}
 	}
 	
