2070a4f7fb6ae510302e948fc17c65e98a7818e9
bbnsdevelop <bbnsdevelop@gmail.com>
2021-11-30 11:03:06 -0800

    Change priority of thread child

--- java-core/src/br/com/bbnsdevelop/threads/groups/ThreadGroupDemo.java
+++ java-core/src/br/com/bbnsdevelop/threads/groups/ThreadGroupDemo.java
@@ -17,6 +17,26 @@ public class ThreadGroupDemo {
 		System.out.println(child.getName());
 		System.out.println(child.getParent().getName());
 		
+		Thread t1 = new Thread(child, "Thread 1");
+		Thread t2 = new Thread(child, "Thread 2");
+		
+		System.out.println(t1.getThreadGroup().getName());
+		System.out.println(t2.getThreadGroup().getName());
+		
+		System.out.println("Thread 1 - priority: " + t1.getPriority());
+		System.out.println("Thread 2 - priority: " + t2.getPriority());
+		
+		child.setMaxPriority(4);
+		Thread t3 = new Thread(child, "Thread 3");
+		Thread t4 = new Thread(child, "Thread 4");
+		
+		System.out.println("Change priority for -------------> 4");		
+		System.out.println("Thread 3 - priority: " + t3.getPriority());
+		System.out.println("Thread 4 - priority: " + t4.getPriority());
+		
+		System.out.println("The priority of objects t1 and t2 don't change because the priority was changed after your creation");
+		System.out.println("Thread 1 - priority: " + t1.getPriority());
+		System.out.println("Thread 2 - priority: " + t2.getPriority());
 		
 	}
 
