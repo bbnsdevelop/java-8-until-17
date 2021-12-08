3188c09860169fc65301980aa7d65a7fc66ca5bd
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-07 22:09:45 -0300

    String methods - split and replace

--- java-core/src/br/com/bbnsdevelop/stringHandling/StringMethods.java
+++ java-core/src/br/com/bbnsdevelop/stringHandling/StringMethods.java
@@ -3,7 +3,7 @@ package br.com.bbnsdevelop.stringHandling;
 public class StringMethods {
 	
 	public static void main(String[] args) {
-		String s = "Hello";
+		String s = "Hello World";
 		
 		System.out.println("Lenght of the string is: "+s.length());
 		System.out.println("IndexOf: "+s.indexOf("o"));
@@ -12,6 +12,21 @@ public class StringMethods {
 		
 		System.out.println("Substring with beginning index: "+s.substring(3));
 		System.out.println("Substring with beginning index: "+s.substring(1, 3));
+		
+		
+		String[] split = s.split(" ");
+		
+		for (int i = 0; i < split.length; i++) {
+			System.out.println(split[i]);
+		}
+		
+		s.replace(" ", "-"); // is immutable
+		
+		System.out.println(s);
+		
+		s = s.replace(" ", "-");
+		System.out.println(s);
+		
 	}
 
 }
