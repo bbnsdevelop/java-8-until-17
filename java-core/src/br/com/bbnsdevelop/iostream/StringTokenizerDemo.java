a4e9dd5d6a6aea44ebc89c990a1559ff945f6255
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-12 21:33:47 -0300

    StringTokenizer

---
+++ java-core/src/br/com/bbnsdevelop/iostream/StringTokenizerDemo.java
@@ -0,0 +1,27 @@
+package br.com.bbnsdevelop.iostream;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizerDemo {
+	
+	public static void main(String[] args) {
+		String s = "You are the creator of your desttiny";
+		
+		StringTokenizer st = new StringTokenizer(s);
+		
+		
+		while (st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
+		}
+		
+		
+		String s1 = "You,are,the,creator,of,your,desttiny";
+		System.out.println("*-------------------------------*");
+		StringTokenizer st1 = new StringTokenizer(s1, ",");
+		
+		while (st1.hasMoreTokens()) {
+			System.out.println(st1.nextToken());
+		}
+	}
+
+}
