02ccfb6f6ef8d11e3ec5df1670a24a10525eca3b
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-16 19:00:54 -0300

    remove if

--- java-core/src/br/com/bbnsdevelop/arrays/ArraysDemo.java
+++ java-core/src/br/com/bbnsdevelop/arrays/ArraysDemo.java
@@ -1,28 +1,25 @@
 package br.com.bbnsdevelop.arrays;
 
 public class ArraysDemo {
-	
+
 	public static void main(String[] args) {
-		
-		int array1[] = {1,2,3,4,5,6,7,8,9,10};
-		
+
+		int array1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
 		System.out.println("Elements from array:");
 		for (int i = 0; i < array1.length; i++) {
 			int element = array1[i];
-			
-			if(element % 2 == 0) {
-				System.out.println("pair number: " + element);				
-			}
+			System.out.println("number: " + element);
 		}
-		
+
 		System.out.println("----------------------------------");
 		int array2[] = new int[5];
 		array2[0] = 1;
 		array2[1] = 2;
 		array2[2] = 3;
 		array2[3] = 4;
-		array2[4] = Integer.valueOf(5); //  Autoboxing
-		
+		array2[4] = Integer.valueOf(5); // Autoboxing
+
 		for (int i = 0; i < array2.length; i++) {
 			int element = array2[i];
 			System.out.println(element);
