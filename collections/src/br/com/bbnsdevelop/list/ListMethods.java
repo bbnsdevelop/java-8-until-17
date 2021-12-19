4ff7451956cf35794e4f0fa90594b8c4b071a7c5
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-19 13:17:34 -0300

    Inserting and Replacing Objects

---
+++ collections/src/br/com/bbnsdevelop/list/ListMethods.java
@@ -0,0 +1,27 @@
+package br.com.bbnsdevelop.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMethods {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<>();
+		
+		for (int i = 10; i <= 100; i = i +10) {
+			list.add(i);
+		}
+	
+		System.out.println(list);
+		
+		list.add(1,25);
+		
+		System.out.println("List after insert: " + list);
+		
+		list.set(2, 28);
+		
+		System.out.println("List after replacement index: " + list);
+	}
+
+}
