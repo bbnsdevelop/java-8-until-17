758d0279ab382f4571152142fe9c0d6a974a315a
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-19 14:58:33 -0300

    add all

---
+++ collections/src/br/com/bbnsdevelop/list/ListMethodsAddAll.java
@@ -0,0 +1,36 @@
+package br.com.bbnsdevelop.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMethodsAddAll {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<>();
+		
+		for (int i = 10; i <= 100; i = i +10) {
+			list.add(i);
+		}
+	
+		System.out.println(list);
+		
+		list.add(1,25);
+		
+		System.out.println("List after insert: " + list);
+		
+		list.set(2, 28);
+		
+		System.out.println("List after replacement index: " + list);
+		
+		List<Integer> secondList = new ArrayList<>();
+		secondList.add(111);
+		secondList.add(222);
+		secondList.add(333);
+		
+		list.addAll(4, secondList);
+		
+		System.out.println("List after add all: " + list);
+	}
+
+}
