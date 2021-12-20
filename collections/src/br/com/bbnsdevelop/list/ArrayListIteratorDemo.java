ed153e4adcbaa9af100a596e123372bd5372607e
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-20 13:22:15 -0800

    work with: Iterator

---
+++ collections/src/br/com/bbnsdevelop/list/ArrayListIteratorDemo.java
@@ -0,0 +1,22 @@
+package br.com.bbnsdevelop.list;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListIteratorDemo {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(10,20,30,40,5,60,50); 
+		
+		Iterator<Integer> itr = list.iterator();
+		
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+			
+		}	
+		
+	}
+
+}
