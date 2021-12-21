616721b4ee7bd137a4b47ef7ff732113974f78c4
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-21 20:23:38 -0300

    Customize object Comparator

---
+++ collections/src/br/com/bbnsdevelop/sortingobjects/EmployeeNameComparator.java
@@ -0,0 +1,15 @@
+package br.com.bbnsdevelop.sortingobjects;
+
+import java.util.Comparator;
+
+public class EmployeeNameComparator implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		String s1 = o1.name;
+		String s2 = o2.name;
+		
+		return s1.compareTo(s2);
+	}
+
+}
---
+++ collections/src/br/com/bbnsdevelop/sortingobjects/EmployeeNameTest.java
@@ -0,0 +1,29 @@
+package br.com.bbnsdevelop.sortingobjects;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class EmployeeNameTest {
+	
+	
+	public static void main(String[] args) {
+		Set<Employee> set = new TreeSet<>(new EmployeeNameComparator());
+		
+		
+		set.add(new Employee(100, "Luiz"));
+		set.add(new Employee(101, "Luiz"));
+		set.add(new Employee(200, "Tom"));
+		set.add(new Employee(400, "Obama"));
+		set.add(new Employee(500, "Ana"));
+		set.add(new Employee(501, "Ana"));
+		set.add(new Employee(300, "Elvis"));
+		set.add(new Employee(600, "Jhon"));
+		
+		
+		
+		for (Employee employee : set) {
+			System.out.println(employee);
+		}
+	}
+
+}
