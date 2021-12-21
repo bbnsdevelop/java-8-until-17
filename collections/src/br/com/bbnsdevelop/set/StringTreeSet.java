4c774fd0072cd3c44ab29f1516a03a4ff308483a
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-21 03:30:12 -0800

    put stringbuffer in correct file

--- collections/src/br/com/bbnsdevelop/set/StringTreeSet.java
+++ collections/src/br/com/bbnsdevelop/set/StringTreeSet.java
@@ -7,22 +7,21 @@ public class StringTreeSet {
 	
 	
 	public static void main(String[] args) {
-		Set<StringBuffer> set = new TreeSet<>();
+
+		Set<String> set = new TreeSet<>();
 		
-		set.add(new StringBuffer("abc"));
-		set.add(new StringBuffer("xyz"));
-		set.add(new StringBuffer("def"));
-		set.add(new StringBuffer("mno"));
-		set.add(new StringBuffer("fgh"));
-		set.add(new StringBuffer("pqr"));
-		set.add(new StringBuffer("abc"));
+		set.add("abc");
+		set.add("xyz");
+		set.add("def");
+		set.add("mno");
+		set.add("fgh");
+		set.add("pqr");
+		set.add("abc");
 		
-		for (StringBuffer element : set) {
+		
+		for (String element : set) {
 			System.out.println(element);
 		}
-		
-		System.out.println("If you use the java version 12 plus this code is done");
-		System.out.println("Other versions throw exception-------> StringBuffer cannot be cast to java.lang.Comparable");
 	}
 
 }
