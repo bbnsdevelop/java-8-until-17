118de7d3ec154fdf4e5b6b3ecf084152d78920fd
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-20 22:30:43 -0300

    StringBuffer - TreeSet

--- collections/src/br/com/bbnsdevelop/set/StringTreeSet.java
+++ collections/src/br/com/bbnsdevelop/set/StringTreeSet.java
@@ -7,21 +7,22 @@ public class StringTreeSet {
 	
 	
 	public static void main(String[] args) {
-		Set<String> set = new TreeSet<>();
+		Set<StringBuffer> set = new TreeSet<>();
 		
-		set.add("abc");
-		set.add("xyz");
-		set.add("def");
-		set.add("mno");
-		set.add("fgh");
-		set.add("pqr");
-		set.add("abc");
+		set.add(new StringBuffer("abc"));
+		set.add(new StringBuffer("xyz"));
+		set.add(new StringBuffer("def"));
+		set.add(new StringBuffer("mno"));
+		set.add(new StringBuffer("fgh"));
+		set.add(new StringBuffer("pqr"));
+		set.add(new StringBuffer("abc"));
 		
-		
-		for (String element : set) {
+		for (StringBuffer element : set) {
 			System.out.println(element);
 		}
 		
+		System.out.println("If you use the java version 12 plus this code is done");
+		System.out.println("Other versions throw exception-------> StringBuffer cannot be cast to java.lang.Comparable");
 	}
 
 }
