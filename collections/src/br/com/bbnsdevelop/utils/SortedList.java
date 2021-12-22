f465a486d3baa372cf86b178f21775947b40dda1
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-22 12:12:53 -0800

    Collections sort

---
+++ collections/src/br/com/bbnsdevelop/utils/SortedList.java
@@ -0,0 +1,33 @@
+package br.com.bbnsdevelop.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortedList {
+
+	
+	public static void main(String[] args) {
+		
+		List<String> l = new ArrayList<>();
+		
+		
+		l.add("L");
+		l.add("A");
+		l.add("C");
+		l.add("X");
+		l.add("W");
+		l.add("Z");
+		l.add("T");
+		
+		
+		System.out.println("Before sorting: " + l);
+		
+		
+		Collections.sort(l);
+		
+		
+		System.out.println("After sorting: " + l);
+		
+	}
+}
