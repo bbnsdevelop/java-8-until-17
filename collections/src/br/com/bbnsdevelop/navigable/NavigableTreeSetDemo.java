e3188e5594b702e587e7b15a447c799f1f584f00
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-22 11:35:24 -0800

    correct code

--- collections/src/br/com/bbnsdevelop/navigable/NavigableTreeSetDemo.java
+++ collections/src/br/com/bbnsdevelop/navigable/NavigableTreeSetDemo.java
@@ -1,48 +1,37 @@
 package br.com.bbnsdevelop.navigable;
 
-import java.util.NavigableMap;
-import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class NavigableTreeSetDemo {
 
-	
 	public static void main(String[] args) {
-		
-		NavigableMap<Integer, String> t = new TreeMap<>();
-		
-		
-		t.put(10, "A");
-		t.put(20, "B");
-		t.put(30, "C");
-		t.put(40, "D");
-		t.put(50, "F");
-		t.put(10, "A");
-		
-		
-		
+
+		TreeSet<Integer> t = new TreeSet<>();
+
+		t.add(10);
+		t.add(20);
+		t.add(30);
+		t.add(40);
+		t.add(50);
+		t.add(10);
+
 		System.out.println(t);
-		
-		
-		System.out.println(t.ceilingKey(30));
-		
-		
-		System.out.println(t.higherKey(10));
+
+		System.out.println(t.ceiling(30));
+
+		System.out.println(t.higher(10));
 		System.out.println(t);
-		
-		System.out.println(t.floorKey(20));
-		
-		
-		System.out.println(t.lowerKey(30));
-		
-		System.out.println(t.pollFirstEntry());
-		
-		
-		System.out.println(t.pollLastEntry());
-		
-		
-		System.out.println(t.descendingMap());
-		
-		
+
+		System.out.println(t.floor(20));
+
+		System.out.println(t.lower(30));
+
+		System.out.println(t.pollFirst());
+
+		System.out.println(t.pollLast());
+
+		System.out.println(t.descendingSet());
+
 		System.out.println(t);
 	}
 }
