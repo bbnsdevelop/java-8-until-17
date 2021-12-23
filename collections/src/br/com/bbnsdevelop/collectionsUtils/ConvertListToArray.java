70903f9570b39959901de2289881f9b1980facea
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-22 23:40:07 -0300

    Convert List to Array

---
+++ collections/src/br/com/bbnsdevelop/collectionsUtils/ConvertListToArray.java
@@ -0,0 +1,33 @@
+package br.com.bbnsdevelop.collectionsUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ConvertListToArray {
+	
+	
+	public static void main(String[] args) {
+		
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		
+		numbers.forEach(n ->{
+			System.out.println("numeros - " + n);
+		});
+		
+		Integer[] arrayNumbers = new Integer [numbers.size()];
+		
+		List<Integer> listNumbers = new ArrayList<Integer>(Arrays.asList(arrayNumbers));
+		listNumbers.add(6);
+		
+		System.out.println(Arrays.toString(arrayNumbers));
+		
+		System.out.println(listNumbers.toString());
+		
+	}
+
+}
