7b03432bb4e01ca59603a8d32705a31546330871
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-24 12:08:49 -0800

    Newest Way To Implementation functional Interface

--- java-8/src/br/com/bbnsdevelop/functionalInterface/Test.java
+++ java-8/src/br/com/bbnsdevelop/functionalInterface/Test.java
@@ -4,10 +4,11 @@ public class Test {
 	
 	
 	public static void main(String[] args) {
-		FuncInterface i = new OldWayToImplementationInterface();		
+		// FuncInterface i = new OldWayToImplementationInterface();		
+		// i.myMethod();
+		
+		FuncInterface i = ()-> System.out.println("Newest Way To Implementation functional Interface");
 		i.myMethod();
-		
-		
 	}
 
 }
