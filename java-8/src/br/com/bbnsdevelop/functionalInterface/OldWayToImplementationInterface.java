c93ea0ced259cc8760bbd1a85947221268236338
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-24 11:52:57 -0800

    Old Way To Implementation Interface

---
+++ java-8/src/br/com/bbnsdevelop/functionalInterface/OldWayToImplementationInterface.java
@@ -0,0 +1,11 @@
+package br.com.bbnsdevelop.functionalInterface;
+
+public class OldWayToImplementationInterface implements FuncInterface{
+
+	@Override
+	public void myMethod() {
+		System.out.println("Old Way To Implementation Interface");
+		
+	}
+
+}
---
+++ java-8/src/br/com/bbnsdevelop/functionalInterface/Test.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop.functionalInterface;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		FuncInterface i = new OldWayToImplementationInterface();		
+		i.myMethod();
+		
+		
+	}
+
+}
