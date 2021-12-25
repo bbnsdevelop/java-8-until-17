574e5b21486b612ab00177b9a7519992e18570e9
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-25 11:44:36 -0800

    Diamond problem with default method in interface

--- java-8/src/br/com/bbnsdevelop/interfaces/defaults/DefaultMethodImpl.java
+++ java-8/src/br/com/bbnsdevelop/interfaces/defaults/DefaultMethodImpl.java
@@ -1,5 +1,10 @@
 package br.com.bbnsdevelop.interfaces.defaults;
 
-public class DefaultMethodImpl implements DefaultMethod{
+public class DefaultMethodImpl implements DefaultMethod, DiamondProblemDefaultMethod{
+
+	@Override
+	public void method1() {
+		System.out.println("Override default method - DefaultMethod");
+	}
 
 }
---
+++ java-8/src/br/com/bbnsdevelop/interfaces/defaults/DiamondProblemDefaultMethod.java
@@ -0,0 +1,9 @@
+package br.com.bbnsdevelop.interfaces.defaults;
+
+public interface DiamondProblemDefaultMethod {
+	
+	default void method1() {
+		System.out.println("method1 - inside interface");
+	}
+
+}
