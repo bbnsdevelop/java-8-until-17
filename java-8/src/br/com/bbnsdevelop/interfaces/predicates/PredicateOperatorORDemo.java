d61981680c3824ab9614eca26a7030344cfe3681
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-25 11:58:48 -0800

    predicate operator OR to verify a logic test

---
+++ java-8/src/br/com/bbnsdevelop/interfaces/predicates/PredicateOperatorORDemo.java
@@ -0,0 +1,20 @@
+package br.com.bbnsdevelop.interfaces.predicates;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PredicateOperatorORDemo {
+
+	public static void main(String[] args) {
+		Predicate<String> lengthIs3 = x -> x.length() == 3;
+		Predicate<String> startWithA = x -> x.startsWith("A");
+
+		List<String> list = Arrays.asList("A", "AA", "AAA", "B", "BB", "BBB", "CC", "CCC");
+		List<String> collect = list.stream().filter(lengthIs3.or(startWithA)).collect(Collectors.toList());
+
+		System.out.println(collect);
+	}
+
+}
