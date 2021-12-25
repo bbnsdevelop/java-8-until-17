089eefc6909651e38c03e304a32c8b9aecc7d1a1
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-25 12:02:13 -0800

    predicate - negation operator

---
+++ java-8/src/br/com/bbnsdevelop/interfaces/predicates/PredicateNegate.java
@@ -0,0 +1,22 @@
+package br.com.bbnsdevelop.interfaces.predicates;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PredicateNegate {
+
+	public static void main(String[] args) {
+
+		Predicate<String> startWithA = x -> x.startsWith("A");
+
+		List<String> list = Arrays.asList("A", "AA", "AAA", "B", "BB", "BBB");
+
+		List<String> collect = list.stream().filter(startWithA.negate()).collect(Collectors.toList());
+
+		System.out.println(collect);
+
+	}
+
+}
