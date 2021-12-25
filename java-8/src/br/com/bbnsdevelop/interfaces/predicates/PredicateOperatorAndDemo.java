560004fbae7e346da37cb827b06e5aec6890f0d0
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-25 11:56:06 -0800

    predicate operator AND to verify a logic test

---
+++ java-8/src/br/com/bbnsdevelop/interfaces/predicates/PredicateOperatorAndDemo.java
@@ -0,0 +1,20 @@
+package br.com.bbnsdevelop.interfaces.predicates;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PredicateOperatorAndDemo {
+
+	public static void main(String[] args) {
+		Predicate<Integer> noGreaterThan5 = x -> x > 5;
+		Predicate<Integer> noLessThan8 = x -> x < 8;
+
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		List<Integer> collect = list.stream().filter(noGreaterThan5.and(noLessThan8)).collect(Collectors.toList());
+
+		System.out.println(collect);
+	}
+
+}
