2dd6d4d0c11dd7d0f2459691e12a925b44d9db3e
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-27 05:12:19 -0800

    Interface reference

---
+++ java-8/src/br/com/bbnsdevelop/reference/MyInterfaceRef.java
@@ -0,0 +1,9 @@
+package br.com.bbnsdevelop.reference;
+
+import br.com.bbnsdevelop.objTotest.Product;
+
+public interface MyInterfaceRef {
+	
+	void showProduct(Product p);
+
+}
---
+++ java-8/src/br/com/bbnsdevelop/reference/TestInterface.java
@@ -0,0 +1,21 @@
+package br.com.bbnsdevelop.reference;
+
+import br.com.bbnsdevelop.objTotest.ProductService;
+
+public class TestInterface {
+
+	public static void main(String[] args) {
+		ProductService service = new ProductService();
+		
+		
+		MyInterfaceRef f = p -> System.out.println(service.getProducts().get(3));		
+		f.showProduct(service.getProducts().get(3));
+
+		f = p -> ProductService.updateValueIfCategoryIsSmartPhone(p);	
+
+		f.showProduct(service.getProducts().get(3));		
+		f = p -> System.out.println(service.getProducts().get(3));		
+		f.showProduct(service.getProducts().get(3));
+	}
+
+}
