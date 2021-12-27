dc67a39b4844e44908d91d74835084bf19202883
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-27 09:09:03 -0800

    min and max quantity

--- java-8/src/br/com/bbnsdevelop/streams/MinAndMax.java
+++ java-8/src/br/com/bbnsdevelop/streams/MinAndMax.java
@@ -6,21 +6,31 @@ import br.com.bbnsdevelop.objTotest.Product;
 import br.com.bbnsdevelop.objTotest.ProductService;
 
 public class MinAndMax {
-	
-	
+
 	public static void main(String[] args) {
 		ProductService service = new ProductService();
-		System.out.println("----------------------------without-comparator-----------------------------------------------------");
-		Comparator<Product> com = (p1,p2) -> p1.getPrice().compareTo(p2.getPrice()); 
-		
+		System.out.println(
+				"----------------------------without-comparator-----------------------------------------------------");
+		Comparator<Product> com = (p1, p2) -> p1.getPrice().compareTo(p2.getPrice());
+
 		Product product = service.getProducts().stream().max(com).get();
-		
-		
+
 		System.out.println("Max price: " + product);
-		
+
 		product = service.getProducts().stream().min(com).get();
-		
+
 		System.out.println("Min price: " + product);
+
+		com = (p1, p2) -> p1.getQuantity().compareTo(p2.getQuantity());
+
+		product = service.getProducts().stream().max(com).get();
+
+		System.out.println("Max Quantity: " + product);
+
+		product = service.getProducts().stream().min(com).get();
+
+		System.out.println("Min Quantity: " + product);
+
 	}
 
 }
