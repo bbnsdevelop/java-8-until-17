6f1e32eb924ad77395c69e8b293cf05e0ce1c622
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-27 19:44:16 -0300

    Consumer

---
+++ java-8/src/br/com/bbnsdevelop/lambdas/basic/Consumers.java
@@ -0,0 +1,20 @@
+package br.com.bbnsdevelop.lambdas.basic;
+
+import java.util.function.Consumer;
+
+import br.com.bbnsdevelop.objTotest.Product;
+import br.com.bbnsdevelop.objTotest.ProductService;
+
+public class Consumers {
+	
+	public static void main(String[] args) {
+		ProductService service = new ProductService();
+		Consumer<Product> print = p -> System.out.println(p.getName());
+		
+		print.accept(service.getProducts().get(0));
+		
+		System.out.println("------------------------");
+		service.getProducts().forEach(print);
+	}
+
+}
