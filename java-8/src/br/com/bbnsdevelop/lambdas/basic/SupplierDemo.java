aa8cf780e2f46f2ccd2fc02ffae8a45afc54722e
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-27 20:37:37 -0300

    Supplier

---
+++ java-8/src/br/com/bbnsdevelop/lambdas/basic/SupplierDemo.java
@@ -0,0 +1,22 @@
+package br.com.bbnsdevelop.lambdas.basic;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import br.com.bbnsdevelop.objTotest.Product;
+import br.com.bbnsdevelop.objTotest.ProductService;
+
+public class SupplierDemo {
+	
+	public static void main(String[] args) {
+		
+		ProductService service = new ProductService();
+		
+		Supplier<List<Product>> list = () -> service.getProducts();
+		
+		Consumer<Product> consumer = p -> System.out.println(p);
+		list.get().forEach(consumer);
+	}
+
+}
