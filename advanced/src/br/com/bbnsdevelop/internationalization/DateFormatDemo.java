0752f27b79a6a0cf52d1f1c4db5a69f640d21990
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-29 20:55:12 -0300

    DateFormat

---
+++ advanced/src/br/com/bbnsdevelop/internationalization/DateFormatDemo.java
@@ -0,0 +1,43 @@
+package br.com.bbnsdevelop.internationalization;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateFormatDemo {
+	
+	public static void main(String[] args) {
+		
+		Date d = new Date();
+		
+		DateFormat usDF = DateFormat.getDateInstance(0, Locale.US);
+		DateFormat ukDF = DateFormat.getDateInstance(0, Locale.UK);
+		
+		System.out.println("US: "+usDF.format(d));		
+		System.out.println("UK: "+ukDF.format(d));
+		
+		System.out.println("----------------------------");
+		
+		usDF = DateFormat.getDateInstance(1, Locale.US);
+		ukDF = DateFormat.getDateInstance(1, Locale.UK);
+		System.out.println("US: "+usDF.format(d));		
+		System.out.println("UK: "+ukDF.format(d));
+		
+		
+		System.out.println("-----------------------------");
+		
+		usDF = DateFormat.getDateInstance(2, Locale.US);
+		ukDF = DateFormat.getDateInstance(2, Locale.UK);
+		System.out.println("US: "+usDF.format(d));		
+		System.out.println("UK: "+ukDF.format(d));
+		
+		
+		System.out.println("------------------------------");
+		usDF = DateFormat.getDateInstance(3, Locale.US);
+		ukDF = DateFormat.getDateInstance(3, Locale.UK);
+		System.out.println("US: "+usDF.format(d));		
+		System.out.println("UK: "+ukDF.format(d));
+		
+	}
+
+}
