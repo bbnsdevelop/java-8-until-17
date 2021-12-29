37914595f718b25bdd4e07eb54fa8809dfeb68d4
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-28 22:16:11 -0300

    Reduce

---
+++ java-8/src/br/com/bbnsdevelop/streams/ReduceDemo.java
@@ -0,0 +1,29 @@
+package br.com.bbnsdevelop.streams;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import br.com.bbnsdevelop.objTotest.CategoryTypes;
+import br.com.bbnsdevelop.objTotest.Product;
+import br.com.bbnsdevelop.objTotest.ProductService;
+
+public class ReduceDemo {
+	
+	public static void main(String[] args) {
+		ProductService service = new ProductService();
+		
+		Predicate<Product> predicate = p -> p.getCategory().equals(CategoryTypes.COMPUTING);
+		
+		Function<Product, Double> functionPriceMultQuant = p -> p.getPrice() * p.getQuantity();
+		BinaryOperator<Double> sum = (x,y) -> x + y;
+		
+		Consumer<Double> consumer = priceFormat -> System.out.print(String.format("%.2f", priceFormat));
+		
+		service.getProducts().stream().filter(predicate).map(functionPriceMultQuant).reduce(sum).ifPresent(consumer);
+		
+		
+	}
+
+}
