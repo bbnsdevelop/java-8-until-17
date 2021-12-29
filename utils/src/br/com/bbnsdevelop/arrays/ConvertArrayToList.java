ef6ab358a82e69d4a1849a1a705f322805c22347
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-29 20:25:43 -0300

    somethings utils

---
+++ utils/src/br/com/bbnsdevelop/arrays/ConvertArrayToList.java
@@ -0,0 +1,36 @@
+package br.com.bbnsdevelop.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ConvertArrayToList {
+	
+	
+	public static void main(String[] args) {
+		
+		String[] array = {"A", "B", "C"};
+		
+		
+		List<String> list = Arrays.asList(array);		
+		
+		System.out.println("get first element array: " + array[0]);
+		System.out.println("get first element list" + list.get(0));
+		
+		System.out.println("------------------------------");
+		System.out.println(array);
+		
+		System.out.println("Reference memory");
+		array[2] = "W";
+		
+		System.out.println(list);
+		
+		System.out.println("Change reference memory");
+		
+		List<String> list1 = new ArrayList<>(Arrays.asList(array));
+		array[1] = "M";
+		System.out.println(list1);
+		
+	}
+
+}
