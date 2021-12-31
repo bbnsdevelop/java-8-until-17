0f035944397f4e8450663a7822b5c9e460a4a3be
bbnsdevelop <bbnsdevelop@gmail.com>
2021-12-30 23:12:57 -0300

    invoke method SET

--- advanced/src/br/com/bbnsdevelop/reflections/TestCalculator.java
+++ advanced/src/br/com/bbnsdevelop/reflections/TestCalculator.java
@@ -19,14 +19,27 @@ public class TestCalculator {
 			Constructor<?> constructor2 = myClass.getConstructor(double.class, double.class);
 			Object newInstance2 = constructor2.newInstance(2.3, 4.6);
 			
+			System.out.println("--------GET--------");
 			Method method = myClass.getMethod("getNum1", null);
-			Object invoke = method.invoke(newInstance2, null);
-			
+			Object invoke = method.invoke(newInstance2, null);			
 			System.out.println(invoke);
 			
 			method = myClass.getMethod("getNum2", null);
-			invoke = method.invoke(newInstance2, null);
+			invoke = method.invoke(newInstance2, null);			
+			System.out.println(invoke);
 			
+			System.out.println("-------SET-GET--------");
+			Method setNum1 = myClass.getMethod("setNum1",double.class);			
+			setNum1.invoke(newInstance2, 51);
+			method = myClass.getMethod("getNum1", null);
+			invoke = method.invoke(newInstance2, null);			
+			System.out.println(invoke);
+			
+			
+			Method setNum2 = myClass.getMethod("setNum2",double.class);			
+			setNum2.invoke(newInstance2, 65);			
+			method = myClass.getMethod("getNum2", null);
+			invoke = method.invoke(newInstance2, null);			
 			System.out.println(invoke);
 			
 		} catch (ClassNotFoundException e) {
