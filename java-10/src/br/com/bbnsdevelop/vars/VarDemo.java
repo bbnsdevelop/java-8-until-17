7bd675a941e51e0276ce525f9778c421f633e565
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-05 11:04:25 -0800

    use var to declare variable

---
+++ java-10/src/br/com/bbnsdevelop/vars/VarDemo.java
@@ -0,0 +1,16 @@
+package br.com.bbnsdevelop.vars;
+
+public class VarDemo {
+
+	public static void main(String[] args) {
+		
+		var x = "abc";
+		System.out.println(x);
+		
+		x = "aasas";
+		
+		System.out.println(x.length());
+
+	}
+
+}
