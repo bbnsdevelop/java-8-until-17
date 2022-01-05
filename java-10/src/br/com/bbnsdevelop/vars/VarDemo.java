312844da7f867e60db75beb2e659c3dab6c15b1c
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-05 11:08:49 -0800

    HashMap use var to declare

--- java-10/src/br/com/bbnsdevelop/vars/VarDemo.java
+++ java-10/src/br/com/bbnsdevelop/vars/VarDemo.java
@@ -1,5 +1,9 @@
 package br.com.bbnsdevelop.vars;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class VarDemo {
 
 	public static void main(String[] args) {
@@ -11,6 +15,12 @@ public class VarDemo {
 		
 		System.out.println(x.length());
 
+		var map = new HashMap<String, List<String>>();
+		
+		map.put("a", Arrays.asList("Alfred", "Ana", "Amelia", "Anthony"));
+		map.put("b", Arrays.asList("Benjamin", "Bryson", "Bentley", "Brandon"));
+		
+		System.out.println(map);
 	}
 
 }
