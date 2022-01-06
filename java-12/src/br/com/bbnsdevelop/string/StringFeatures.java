4a412909832189deaf1d4b082184788f50499f66
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-06 20:56:43 -0300

    String method indent

---
+++ java-12/src/br/com/bbnsdevelop/string/StringFeatures.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop.string;
+
+public class StringFeatures {
+	
+	public static void main(String[] args) {
+		String str = "All the power is with in you. You can do anything and everything";
+		System.out.println(str);
+		System.out.println("-".repeat(50));
+		System.out.println(str.indent(5));
+		
+	}
+
+}
