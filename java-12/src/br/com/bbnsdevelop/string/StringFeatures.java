b525e7d4d92e0521114325dc8d647179d51cca3a
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-06 20:59:19 -0300

    String method transform

--- java-12/src/br/com/bbnsdevelop/string/StringFeatures.java
+++ java-12/src/br/com/bbnsdevelop/string/StringFeatures.java
@@ -8,6 +8,13 @@ public class StringFeatures {
 		System.out.println("-".repeat(50));
 		System.out.println(str.indent(5));
 		
+		
+		String s1 = "10";
+		
+		Integer result = s1.transform(Integer::parseInt);
+		System.out.println(result instanceof Integer);
+		System.out.println(result);
+		
 	}
 
 }
