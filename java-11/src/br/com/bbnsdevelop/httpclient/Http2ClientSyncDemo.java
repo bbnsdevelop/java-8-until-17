0a5f5f12f9a08849d473fac8e435c8fce3fb0a19
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-06 10:53:46 -0800

    Http Cliente - POST

--- java-11/src/br/com/bbnsdevelop/httpclient/Http2ClientSyncDemo.java
+++ java-11/src/br/com/bbnsdevelop/httpclient/Http2ClientSyncDemo.java
@@ -5,6 +5,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.stream.Stream;
@@ -26,6 +28,21 @@ public class Http2ClientSyncDemo {
 		System.out.println(response.statusCode());
 		Stream<String> body = response.body();
 		
+		body.forEach( a -> System.out.println(a));
+		
+		
+		System.out.println("-".repeat(40));
+		BodyPublisher bodyPublisher = BodyPublishers.ofString("{\r\n"
+				+ "  \"id\": 0,\r\n"
+				+ "  \"title\": \"string\",\r\n"
+				+ "  \"dueDate\": \"2022-01-06T18:46:37.804Z\",\r\n"
+				+ "  \"completed\": true\r\n"
+				+ "}");
+		
+		request = HttpRequest.newBuilder(uri).POST(bodyPublisher).header("Content-Type","application/json").build();
+		response = http.send(request, BodyHandlers.ofLines());				
+		System.out.println(response.statusCode());
+		body = response.body();		
 		body.forEach( a -> System.out.println(a));
 		
 	}
