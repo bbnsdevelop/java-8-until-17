fa161c0b943e742b960990c81e05fac59420cdf4
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-06 21:48:02 -0300

    unicode chars

---
+++ java-12/src/br/com/bbnsdevelop/unicodechars/Chess.java
@@ -0,0 +1,14 @@
+package br.com.bbnsdevelop.unicodechars;
+
+public class Chess {
+	
+	public static void main(String[] args) {
+		System.out.println("\u265A");
+		System.out.println("\u265B");
+		
+		
+		System.out.println("\u2654");
+		System.out.println("\u2655");
+	}
+
+}
