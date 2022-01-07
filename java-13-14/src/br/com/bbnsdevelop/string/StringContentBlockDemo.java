d49da790485e58165a720ca7ea6178050338e9ea
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-07 12:48:47 -0800

    String content block

---
+++ java-13-14/src/br/com/bbnsdevelop/string/StringContentBlockDemo.java
@@ -0,0 +1,20 @@
+package br.com.bbnsdevelop.string;
+
+public class StringContentBlockDemo {
+	
+	public static void main(String[] args) {
+		
+		String s = """
+				 <html>
+				  <body>
+				 	<p>Hello, world</p>
+				   </body>
+				 </html>
+				 
+				 """;
+		
+		
+		System.out.println(s);
+	}
+
+}
