56366d42888ed14a4772c6cffe0a0afdbe5d4704
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-09 17:43:09 -0300

    Right Angled Triangle pattern

---
+++ data-structure/src/br/com/bbnsdevelop/RightAngledTriangle.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop;
+
+public class RightAngledTriangle {
+	
+	public static void main(String[] args) {
+		
+		int n = 8;		
+		for (int i = 1; i <=n; i++) {
+			System.out.println("* ".repeat(i));
+		}
+	}
+
+}
