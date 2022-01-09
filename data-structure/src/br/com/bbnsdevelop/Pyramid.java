bce7a01cacd49345b25ee3db9ccca34c054d6f3d
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-09 17:55:19 -0300

    Pyramid

---
+++ data-structure/src/br/com/bbnsdevelop/Pyramid.java
@@ -0,0 +1,23 @@
+package br.com.bbnsdevelop;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+
+		int n = 10;
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= (n-i); j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 1; k <= i; k++) {
+				System.out.print("* ");
+			}
+
+			System.out.println();
+		}
+	}
+
+}
