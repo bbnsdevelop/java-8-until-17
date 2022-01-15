9b36c60f0929d47eaab64f6f86809b2ddc1900eb
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-15 20:10:13 -0300

    Design pattern - Singleton

---
+++ DesignPatterns/src/br/com/bbnsdevelop/v2_examples/singleton/DateUtil.java
@@ -0,0 +1,26 @@
+package br.com.bbnsdevelop.v2_examples.singleton;
+
+import java.util.Date;
+
+public class DateUtil {
+
+	private static DateUtil instance;
+	private Date date;
+
+	private DateUtil(Date date) {
+		this.date = date;
+	}
+
+	public static DateUtil getInstance(Date date) {
+		if(instance == null) {
+			instance = new DateUtil(date);			
+		}
+		return instance;
+
+	}
+	
+	public Date getDate() {
+		return this.date;
+	}
+
+}
---
+++ DesignPatterns/src/br/com/bbnsdevelop/v2_examples/singleton/DateUtilDemo.java
@@ -0,0 +1,19 @@
+package br.com.bbnsdevelop.v2_examples.singleton;
+
+import java.util.Date;
+
+public class DateUtilDemo {
+	
+	public static void main(String[] args) {
+		
+		DateUtil date1 = DateUtil.getInstance(new Date());
+		DateUtil date2 = DateUtil.getInstance(new Date(30/12/2021));
+		
+		System.out.println(date1.getDate());
+		System.out.println(date1.getDate());
+		
+		System.out.println("Same information");
+		System.out.println(date1 == date2);
+	}
+
+}
