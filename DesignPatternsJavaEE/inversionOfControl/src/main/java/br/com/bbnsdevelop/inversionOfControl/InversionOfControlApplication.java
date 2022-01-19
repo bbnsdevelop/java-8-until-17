a01a9316d1e69e2f7f6ccd850c22fc2396561547
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-18 23:04:16 -0300

    create a spring project

---
+++ DesignPatternsJavaEE/inversionOfControl/src/main/java/br/com/bbnsdevelop/inversionOfControl/InversionOfControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop.inversionOfControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InversionOfControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InversionOfControlApplication.class, args);
+	}
+
+}
---
+++ DesignPatternsJavaEE/inversionOfControl/src/test/java/br/com/bbnsdevelop/inversionOfControl/InversionOfControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop.inversionOfControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InversionOfControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
