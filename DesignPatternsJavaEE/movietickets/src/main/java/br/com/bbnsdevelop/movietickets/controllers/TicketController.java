d8b04708539bbdb0df49dd69799ab9a2464187bf
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-21 18:51:20 -0300

    Handle Ticket Creation

--- DesignPatternsJavaEE/movietickets/src/main/java/br/com/bbnsdevelop/movietickets/controllers/TicketController.java
+++ DesignPatternsJavaEE/movietickets/src/main/java/br/com/bbnsdevelop/movietickets/controllers/TicketController.java
@@ -1,13 +1,25 @@
 package br.com.bbnsdevelop.movietickets.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.bbnsdevelop.movietickets.dao.TicketDAO;
+import br.com.bbnsdevelop.movietickets.entities.Ticket;
 
 @Controller
 public class TicketController {
 	
+	@Autowired
+	private TicketDAO dao;
+	
 	@RequestMapping("/showCreateTicket")
 	public String showPageCreateTicket() {
+		return "createTicket";
+	}
+	
+	public String createTicket(Ticket ticket) {
+		dao.create(ticket);
 		return "createTicket";
 	}
 
