cadfbbb8a32de7f8934ab6fc1d33322b5da58b68
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-21 12:59:15 -0800

    Create a mini project to practice design pattern

---
+++ DesignPatternsJavaEE/movietickets/src/main/java/br/com/bbnsdevelop/movietickets/MovieticketsApplication.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop.movietickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieticketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieticketsApplication.class, args);
+	}
+
+}
