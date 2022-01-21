4388eff9b31c697f60339661898632296eb0a783
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-21 13:07:59 -0800

    create a representation of an entity Ticket

---
+++ DesignPatternsJavaEE/movietickets/src/main/java/br/com/bbnsdevelop/movietickets/entities/Ticket.java
@@ -0,0 +1,60 @@
+package br.com.bbnsdevelop.movietickets.entities;
+
+public class Ticket {
+	
+	private int id;
+	private String movie;
+	private String screen;
+	private String seat;
+	
+	public Ticket() {}
+
+	public Ticket(int id, String movie, String screen, String seat) {
+		this.id = id;
+		this.movie = movie;
+		this.screen = screen;
+		this.seat = seat;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMovie() {
+		return movie;
+	}
+
+	public void setMovie(String movie) {
+		this.movie = movie;
+	}
+
+	public String getScreen() {
+		return screen;
+	}
+
+	public void setScreen(String screen) {
+		this.screen = screen;
+	}
+
+	public String getSeat() {
+		return seat;
+	}
+
+	public void setSeat(String seat) {
+		this.seat = seat;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+}
