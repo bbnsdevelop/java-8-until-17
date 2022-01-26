44a54dd5482ab5bd004962b09e0ef350fe92640d
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-25 21:03:38 -0300

    declarer field for class Board

--- game-minesweeper/src/br/com/bbnsdevelop/minesweeper/model/Board.java
+++ game-minesweeper/src/br/com/bbnsdevelop/minesweeper/model/Board.java
@@ -1,5 +1,15 @@
 package br.com.bbnsdevelop.minesweeper.model;
 
-public class Board {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Board {
+	private int qtdLines;
+	private int qtdColun;
+	private int qtdmines;
+	
+	private final List<Field> fields = new ArrayList<>();
+	
+	
+	
 }
