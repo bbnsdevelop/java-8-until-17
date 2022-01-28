5ce9034d9a54d55fdbcde255a4c4e877809dd815
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-28 18:36:03 -0300

    delete test sql

--- sql-for-jpa/create-table-states.sql
+++
@@ -1,9 +0,0 @@
--- create table states
-CREATE TABLE states(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name_states VARCHAR(45) UNIQUE NOT NULL,
-    initials VARCHAR(2) UNIQUE NOT NULL,
-    region ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
-    populations DECIMAL(5,2) NOT NULL,
-    PRIMARY KEY (id)
-);
