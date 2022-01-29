59f0166a280d6814122e0f838e80ad5d34ba0af6
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-29 11:10:50 -0300

    update data with JDBC

--- jdbc/src/br/com/bbnsdevelop/jdbc/tables/Main.java
+++ jdbc/src/br/com/bbnsdevelop/jdbc/tables/Main.java
@@ -2,6 +2,7 @@ package br.com.bbnsdevelop.jdbc.tables;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 import br.com.bbnsdevelop.jdbc.tables.entities.People;
 
@@ -24,6 +25,24 @@ public class Main {
 		People person = service.findById(1);
 		
 		System.out.println(person);
+		
+		System.out.println("-".repeat(50));
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Typing id: ");
+		int id = Integer.valueOf(in.nextLine());
+		
+		System.out.println("Typing new name: ");
+		String name = in.nextLine();
+		
+		
+		People newPeople = new People(id, name);
+		if(name != null && !name.isEmpty()) {
+			String result = service.updatePeople(newPeople);
+			System.out.println(result);			
+		}
+		
 	}
 
 }
--- jdbc/src/br/com/bbnsdevelop/jdbc/tables/ServicePeople.java
+++ jdbc/src/br/com/bbnsdevelop/jdbc/tables/ServicePeople.java
@@ -12,44 +12,56 @@ import br.com.bbnsdevelop.jdbc.factory.ConnectionFactory;
 import br.com.bbnsdevelop.jdbc.tables.entities.People;
 
 public class ServicePeople {
-	
+
 	private Connection connection;
-	
-	
+
 	public ServicePeople() {
 		connection = ConnectionFactory.getConnection();
 	}
-	
-	public List<People> findAll() throws SQLException{
+
+	public List<People> findAll() throws SQLException {
 		String sql = "SELECT * FROM people";
-		
+
 		Statement stmt = connection.createStatement();
 		ResultSet resultSet = stmt.executeQuery(sql);
-		
+
 		List<People> peolpe = new ArrayList<>();
-		
-		while(resultSet.next()) {
+
+		while (resultSet.next()) {
 			int id = resultSet.getInt("id");
-			String name = resultSet.getString("name");			
+			String name = resultSet.getString("name");
 			peolpe.add(new People(id, name));
-		}				
+		}
 		return peolpe;
 	}
-	
+
 	public People findById(int id) throws SQLException {
 		String sql = "SELECT * FROM people WHERE id = ?";
 		PreparedStatement stmt = connection.prepareStatement(sql);
 		stmt.setInt(1, id);
-		
+
 		ResultSet resultSet = stmt.executeQuery();
 		People person = null;
-		
+
 		while (resultSet.next()) {
 			id = resultSet.getInt("id");
 			String name = resultSet.getString("name");
 			person = new People(id, name);
-		}		
-		return person;		
+		}
+		return person;
+	}
+
+	public String updatePeople(People newPeople) throws SQLException {
+		People p = findById(newPeople.getId());
+
+		String sql = "UPDATE people SET name = ? WHERE id = ?";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, newPeople.getName());
+		stmt.setInt(2, p.getId());
+		stmt.execute();
+		
+		return "Successfully to update a person ".concat(newPeople.getName());
+		
 	}
 
 }
