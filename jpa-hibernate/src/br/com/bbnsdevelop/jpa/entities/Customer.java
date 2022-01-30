39713863849c808c0137735802de0aec9183ea01
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-30 18:42:37 -0300

    Annotation -> @OneToOne

---
+++ jpa-hibernate/src/br/com/bbnsdevelop/jpa/entities/Customer.java
@@ -0,0 +1,62 @@
+package br.com.bbnsdevelop.jpa.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@OneToOne
+	@JoinColumn(name="seat_id" ,nullable = false, unique = true)
+	private Seat seat;
+
+	public Customer() {}
+
+	public Customer(String name, Seat seat) {
+		this.name = name;
+		this.seat = seat;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Seat getSeat() {
+		return seat;
+	}
+
+	public void setSeat(Seat seat) {
+		this.seat = seat;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + "]";
+	}
+	
+
+}
