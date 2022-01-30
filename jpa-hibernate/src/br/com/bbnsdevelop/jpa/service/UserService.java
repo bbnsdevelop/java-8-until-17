9d0fd90e3f7ddaf8a25c0b230ea896e802f07f50
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-30 13:54:26 -0300

    create new functionalities

--- jpa-hibernate/src/br/com/bbnsdevelop/jpa/service/UserService.java
+++ jpa-hibernate/src/br/com/bbnsdevelop/jpa/service/UserService.java
@@ -1,8 +1,11 @@
 package br.com.bbnsdevelop.jpa.service;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import br.com.bbnsdevelop.jpa.entities.User;
 
@@ -21,11 +24,19 @@ public class UserService {
 		manager.getTransaction().begin();
 		manager.persist(user);
 		manager.getTransaction().commit();
-		return "Successfully to save the user in database";
+		return "Successfully to save the user in database id: " + user.getId();
 	}
 	
 	public User findUserById(Long id) {
 		return manager.find(User.class, id);
+	}
+	
+	public List<User> findAll(){		
+		String jpql = "select u from User u";		
+		TypedQuery<User> query = manager.createQuery(jpql, User.class);		
+		List<User> users = query.getResultList();
+		
+		return users;
 	}
 	
 
