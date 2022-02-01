6a806c36b234dabe0d40230abb702a951e5a661e
bbnsdevelop <bbnsdevelop@gmail.com>
2022-01-31 22:42:29 -0300

    refactory - add menu

---
+++ jpa-hibernate/src/br/com/bbnsdevelop/jpa/view/Menu.java
@@ -0,0 +1,18 @@
+package br.com.bbnsdevelop.jpa.view;
+
+public class Menu {
+	
+	public static void showMenu() {
+		System.out.println("1 - create new User");
+		System.out.println("2 - Find all");
+		System.out.println("3 - Find by id");
+		System.out.println("4 - Update by id");
+		System.out.println("5 - Delete by id");
+		System.out.println("6 - Make reservation");
+		System.out.println("7 - Find Customer and Seat");
+		System.out.println("8 - Buy item");
+		System.out.println("99 - exit");
+		System.out.println("Choose a options: ");
+	}
+
+}
--- jpa-hibernate/src/br/com/bbnsdevelop/jpa/view/Process.java
+++ jpa-hibernate/src/br/com/bbnsdevelop/jpa/view/Process.java
@@ -22,16 +22,7 @@ public class Process {
 	
 	public static int operations() {
 		Scanner in = new Scanner(System.in);
-		System.out.println("1 - create new User");
-		System.out.println("2 - Find all");
-		System.out.println("3 - Find by id");
-		System.out.println("4 - Update by id");
-		System.out.println("5 - Delete by id");
-		System.out.println("6 - Make reservation");
-		System.out.println("7 - Find Customer and Seat");
-		System.out.println("8 - Buy item");
-		System.out.println("99 - exit");
-		System.out.println("Choose a options: ");
+		Menu.showMenu();
 		int option = Integer.valueOf(in.nextLine());
 		return option;
 	}	
