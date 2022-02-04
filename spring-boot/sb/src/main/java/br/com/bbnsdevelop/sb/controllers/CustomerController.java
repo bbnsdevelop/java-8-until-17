15d4ab52b3c4888efda5d8879e92dbdfbc4a53c1
bbnsdevelop <bbnsdevelop@gmail.com>
2022-02-04 15:36:48 -0300

    @RequestParam

--- spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/CustomerController.java
+++ spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/CustomerController.java
@@ -1,8 +1,11 @@
 package br.com.bbnsdevelop.sb.controllers;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bbnsdevelop.sb.models.Customer;
@@ -11,13 +14,19 @@ import br.com.bbnsdevelop.sb.models.Customer;
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	@GetMapping
+	@GetMapping("/any")
 	public Customer getCustomer() {
 		return new Customer(1, "John", "123456789");
 	}
 	
 	@GetMapping("/{id}")
-	public Customer getCustomer(@PathVariable("id") Integer id) {
+	public Customer getCustomer1(@PathVariable("id") Integer id) {
+		return new Customer(id, "John", "123456789");
+	}
+	
+	
+	@GetMapping
+	public Customer getCustomer2(@RequestParam(name = "id", required = true) Integer id) {
 		return new Customer(id, "John", "123456789");
 	}
 
