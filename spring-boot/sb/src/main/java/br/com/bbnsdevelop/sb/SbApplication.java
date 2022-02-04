20166eadd41e1ef18ece8ec0f6b7606cd102138f
bbnsdevelop <bbnsdevelop@gmail.com>
2022-02-04 12:48:50 -0300

    create project with spring-boot

---
+++ spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/SbApplication.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApplication.class, args);
+	}
+
+}
---
+++ spring-boot/sb/src/test/java/br/com/bbnsdevelop/sb/SbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
