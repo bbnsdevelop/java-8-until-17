3ab5a8dca2bf275db3a79843814394b845777251
bbnsdevelop <bbnsdevelop@gmail.com>
2022-02-04 15:26:48 -0300

    @PathVariable

--- spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/CustomerController.java
+++ spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/CustomerController.java
@@ -1,6 +1,7 @@
 package br.com.bbnsdevelop.sb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class CustomerController {
 	@GetMapping
 	public Customer getCustomer() {
 		return new Customer(1, "John", "123456789");
+	}
+	
+	@GetMapping("/{id}")
+	public Customer getCustomer(@PathVariable("id") Integer id) {
+		return new Customer(id, "John", "123456789");
 	}
 
 }
