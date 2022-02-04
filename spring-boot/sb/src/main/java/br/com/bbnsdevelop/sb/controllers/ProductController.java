86e76d38c53132fc8313b486e8c7ed54723a3279
bbnsdevelop <bbnsdevelop@gmail.com>
2022-02-04 18:34:54 -0300

    get all product

--- spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/ProductController.java
+++ spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/ProductController.java
@@ -1,8 +1,12 @@
 package br.com.bbnsdevelop.sb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,14 @@ public class ProductController {
 		productRepository.save(p);
 		return p;
 	}
+	
+	@GetMapping
+	public List<Product> getAll(){
+		Iterable<Product> all = productRepository.findAll();
+		List<Product> products = new ArrayList<>();
+		all.forEach(products::add);
+		return products;
+	}
+	
 
 }
