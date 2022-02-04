786295f69f4f279efc9310e5c37e3ce066d02fc9
bbnsdevelop <bbnsdevelop@gmail.com>
2022-02-04 18:35:15 -0300

    get by id

--- spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/ProductController.java
+++ spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/ProductController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,10 @@ public class ProductController {
 		return products;
 	}
 	
+
+	@GetMapping("/{id}")
+	public Product getById(@PathVariable("id") Long id ) {
+		return productRepository.findById(id).orElse(null);
+	}
 
 }
