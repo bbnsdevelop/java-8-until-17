e368bb70eb0a00c75efbbf8fc0bf47aaa506f770
bbnsdevelop <bbnsdevelop@gmail.com>
2022-02-04 14:41:46 -0300

    return an object JSON

---
+++ spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/models/Customer.java
@@ -0,0 +1,44 @@
+package br.com.bbnsdevelop.sb.models;
+
+public class Customer {
+	
+	
+	private Integer id;
+	private String name;
+	private String cpf;
+
+	public Customer() {
+	}
+
+	public Customer(Integer id, String name, String cpf) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+
+}
