384fe870f70efc73da98439b4e5c4b16d23b11f9
bbnsdevelop <bbnsdevelop@gmail.com>
2022-02-05 15:42:42 -0300

    Pageable

--- spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/ProductController.java
+++ spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/controllers/ProductController.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bbnsdevelop.sb.model.entities.Product;
@@ -23,6 +26,7 @@ import br.com.bbnsdevelop.sb.model.repository.ProductRepository;
 @RequestMapping("/api/products")
 public class ProductController {
 	
+	private static final int MAX_ITEM = 50;
 	@Autowired
 	private ProductRepository productRepository;
 	
@@ -40,6 +44,13 @@ public class ProductController {
 		return products;
 	}
 	
+	@GetMapping("/page")
+	public Iterable<Product> getProducts(@RequestParam("number") Integer number, @RequestParam("quantity") Integer quantity ){
+		quantity = quantity > MAX_ITEM ? MAX_ITEM : quantity;
+		Pageable pageable = PageRequest.of(number, quantity);
+				
+		return productRepository.findAll(pageable);
+	}
 
 	@GetMapping("/{id}")
 	public Product getById(@PathVariable("id") Long id ) {
--- spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/model/repository/ProductRepository.java
+++ spring-boot/sb/src/main/java/br/com/bbnsdevelop/sb/model/repository/ProductRepository.java
@@ -1,11 +1,11 @@
 package br.com.bbnsdevelop.sb.model.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.bbnsdevelop.sb.model.entities.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long>{
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long>{
 
 }
