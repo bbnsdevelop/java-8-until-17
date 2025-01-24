94a9ce174b7b141210a1ab963670d3e3f3f94203
bbnsdevelop <bbnsdevelop@gmail.com>
2025-01-24 18:49:25 -0300

    remove comments

--- java-8/src/br/com/bbnsdevelop/functionalInterface/basics/Test.java
+++ java-8/src/br/com/bbnsdevelop/functionalInterface/basics/Test.java
@@ -4,11 +4,11 @@ public class Test {
 	
 	
 	public static void main(String[] args) {
-		// FuncInterface i = new OldWayToImplementationInterface();		
-		// i.myMethod();
+		 FuncInterface i = new OldWayToImplementationInterface();		
+		 i.myMethod();
 		
-		FuncInterface i = ()-> System.out.println("Newest Way To Implementation functional Interface");
-		i.myMethod();
+		FuncInterface f = ()-> System.out.println("Newest Way To Implementation functional Interface");
+		f.myMethod();
 	}
 
 }
